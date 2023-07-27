c63b5902d236d865c7243b848fbf6aa9453d0917
Ruma303 <matteoh8@hotmail.it>
2023-07-27 20:29:58 +0200

    aggiunte funzioni anonime

--- anonymous_function.php
+++ anonymous_function.php
@@ -15,3 +15,30 @@ echo $nome;  // Matteo */
     echo 'Hello World!';
 };
 $fn(); */
+
+
+
+//% First Class Functions
+
+//$ Assegnare una funzione a una variabile
+/* $saluta = function($nome) {
+return "Ciao, $nome!";
+};
+echo $saluta("Mario");  // Ciao, Mario!" */
+
+//$ Closure: Passare una funzione come argomento a un'altra funzione
+
+/* function funzioneSuperiore($callback, 5) {
+    echo "Funzione anonima \$funzioneSuperiore() eseguita.<br>";
+    return $callback(5);
+} */
+
+   /*  function funzioneSuperiore($funzione, $valore) {
+        echo "Funzione anonima \$funzioneSuperiore() eseguita.<br>";
+        return $funzione($valore);
+    }
+    $callback = function($numero) {
+        echo "Funzione anonima \$callback() eseguita.<br>";
+        return $numero * 2;
+    };
+    echo funzioneSuperiore($callback, 5); */
