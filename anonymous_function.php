22694bdd3d4f06420497a0d2f0822cdd75c7580c
Ruma <matteoh8@hotmail.it>
2023-08-04 09:43:55 +0200

    closure ok

--- anonymous_function.php
+++ anonymous_function.php
@@ -10,39 +10,60 @@ $nome = "Matteo";
 echo $nome;  // Matteo */
 
 
-//$Funzioni assegnate a variabili
-/* $fn = function () {
-    echo 'Hello World!';
-};
-$fn(); */
-
-
 
 //% First Class Functions
 
-//$ Assegnare una funzione a una variabile
+//$ Funzioni assegnate a variabili
 /* $saluta = function($nome) {
 return "Ciao, $nome!";
 };
 echo $saluta("Mario");  // Ciao, Mario!" */
 
+
 //$ Callback: Passare una funzione come argomento a un'altra funzione
+/* function funzioneSuperiore($callback, 5) {
+    echo "Funzione anonima \$funzioneSuperiore() eseguita.<br>";
+    return $callback(5);
+} */
 
-    /* function funzioneSuperiore($callback, 5) {
-        echo "Funzione anonima \$funzioneSuperiore() eseguita.<br>";
-        return $callback(5);
-    } */
+/* function funzioneSuperiore(callable $funzione, $valore) {
+    //var_dump($funzione);
+    echo "Funzione anonima \$funzioneSuperiore() eseguita.<br>";
+    return $funzione($valore);
+}
+$callback = function($numero) {
+    echo "Funzione anonima \$callback() eseguita.<br>";
+    return $numero * 2;
+};
+echo funzioneSuperiore($callback, 10); */
 
-    /* function funzioneSuperiore(callable $funzione, $valore) {
-        //var_dump($funzione);
-        echo "Funzione anonima \$funzioneSuperiore() eseguita.<br>";
-        return $funzione($valore);
-    }
-    $callback = function($numero) {
-        echo "Funzione anonima \$callback() eseguita.<br>";
-        return $numero * 2;
-    };
-    echo funzioneSuperiore($callback, 10); */
 
+
+//% Closure
+/* $saluto = "Ciao, mondo!";
+$funzione = function() use ($saluto) {
+    return $saluto;
+};
+echo $funzione(); // Ciao, mondo!
+ */
 
 //% Arrow function
+
+//$var = fn(parameters) => expression;
+
+/* $saluto = "Ciao, mondo!";
+$funzione = fn() => $saluto;
+echo $funzione(); // Ciao, mondo! */
+
+/* $factor = 10;
+$array = [1, 2, 3, 4, 5];
+$arrowFunction = array_map(fn($val) => $val * $factor, $array);
+
+$closure = array_map(function($val) use ($factor) {
+    return $val * $factor;
+}, $array);
+
+echo 'Arrow Function: <br>';
+print_r($arrowFunction);
+echo 'Closure: <br>';
+print_r($closure); */
